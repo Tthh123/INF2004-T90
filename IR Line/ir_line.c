c38e5ceac4943462750dc5885a40d95ad53445b9
nickphoon <potatonicholas@gmail.com>
2023-11-19 01:24:19 +0800

    added ir line helper functions

--- IR Line/ir_line.c
+++ IR Line/ir_line.c
@@ -19,7 +19,8 @@ const uint8_t RIGHT_ADC_CHANNEL = 1;
 
 const uint16_t BLACK_SURFACE_THRESHOLD = 1000;
 
-void init_gpio() {
+void init_ir() {
+    adc_init();
     // Initialize IR sensor and power supply pins
     const uint8_t IR_SENSOR_PINS[] = {LEFT_IR_SENSOR, RIGHT_IR_SENSOR};
     const uint8_t IR_SENSOR_VCC[] = {L_VCC, R_VCC};
@@ -42,45 +43,70 @@ void init_gpio() {
     }
 }
 
-void read_lines() {
-    // Initialize ADC
-    adc_init();
+
+// void read_lines() {
+//     // Initialize ADC
+//     adc_init();
     
-    while (true) {
-        // Read analog values
-        adc_select_input(LEFT_ADC_CHANNEL);
-        uint16_t left_adc_result = adc_read();
-        adc_select_input(RIGHT_ADC_CHANNEL);
-        uint16_t right_adc_result = adc_read();
+//     while (true) {
+//         // Read analog values
+//         adc_select_input(LEFT_ADC_CHANNEL);
+//         uint16_t left_adc_result = adc_read();
+//         adc_select_input(RIGHT_ADC_CHANNEL);
+//         uint16_t right_adc_result = adc_read();
 
-        // Read digital states
-        bool left_ir_state = gpio_get(D_LEFT_IR_SENSOR);
-        bool right_ir_state = gpio_get(D_RIGHT_IR_SENSOR);
+//         // Read digital states
+//         bool left_ir_state = gpio_get(D_LEFT_IR_SENSOR);
+//         bool right_ir_state = gpio_get(D_RIGHT_IR_SENSOR);
         
-        // Determine surface color based on ADC values
-        const char* left_surface = (left_adc_result > BLACK_SURFACE_THRESHOLD) ? "Black" : "White";
-        const char* right_surface = (right_adc_result > BLACK_SURFACE_THRESHOLD) ? "Black" : "White";
+//         // Determine surface color based on ADC values
+//         const char* left_surface = (left_adc_result > BLACK_SURFACE_THRESHOLD) ? "Black" : "White";
+//         const char* right_surface = (right_adc_result > BLACK_SURFACE_THRESHOLD) ? "Black" : "White";
 
-        const char* format_string_left = "Left ADC Result: %d - State: %d - Surface: %s\n";
-        const char* format_string_right = "Right ADC Result: %d - State: %d - Surface: %s\n";
-        // Print results
-        printf(format_string_left, left_adc_result, left_ir_state, left_surface);
-        printf(format_string_right, right_adc_result, right_ir_state, right_surface);
+//         const char* format_string_left = "Left ADC Result: %d - State: %d - Surface: %s\n";
+//         const char* format_string_right = "Right ADC Result: %d - State: %d - Surface: %s\n";
+//         // Print results
+//         printf(format_string_left, left_adc_result, left_ir_state, left_surface);
+//         printf(format_string_right, right_adc_result, right_ir_state, right_surface);
 
-        // Sample every 1s
-        sleep_ms(1000);
-    }
+//         // Sample every 1s
+//         sleep_ms(1000);
+//     }
+// }
+
+bool is_left_surface_black() {
+
+    // Read analog values
+    adc_select_input(LEFT_ADC_CHANNEL);
+    uint16_t left_adc_result = adc_read();
+
+    // Determine surface color based on ADC values
+    bool left_surface_black = (left_adc_result > BLACK_SURFACE_THRESHOLD); // return 1 if black, else return 0 if white
+
+    return left_surface_black;
 }
+
+bool is_right_surface_black() {
+
+    // Read analog values
+    adc_select_input(RIGHT_ADC_CHANNEL);
+    uint16_t right_adc_result = adc_read();
+
+    // Determine surface color based on ADC values
+    bool right_surface_black = (right_adc_result > BLACK_SURFACE_THRESHOLD); 
+
+    return right_surface_black; // return 1 if black, else return 0 if white
+} 
 
 int main(void) {
     // Initialize stdio
     stdio_init_all();
 
     // Initialize GPIO
-    init_gpio();
+    init_ir();
 
     // Start reading sensor values
-    read_lines();
+    // read_lines();
 
     return 0;
 }
