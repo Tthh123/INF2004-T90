4ac63a6dc5a6ab97fd26eed7fee8945af4d8582a
Travis <61496540+Tthh123@users.noreply.github.com>
2023-11-30 23:27:00 +0800

    Update motor.c

--- Drivers/Motor/motor.c
+++ Drivers/Motor/motor.c
@@ -20,65 +20,6 @@
 #define RIGHT_FORWARD  3
 #define LEFT_BACKWARD  4
 #define LEFT_FORWARD   5
-#define TRIG_PIN 6
-#define ECHO_PIN 7
-
-volatile uint32_t start_time = 0;
-volatile uint32_t end_time = 0;
-volatile bool new_measurement_available = false;
-
-const uint32_t wrap_speed = 64515;
-const float left_adjustments = 0.965;
-const float turn_value = 0.0;
-
-void echo_isr() {
-    if (gpio_get(ECHO_PIN)) { // Rising edge detected
-        start_time = time_us_32();
-    } else { // Falling edge detected
-        end_time = time_us_32();
-        new_measurement_available = true;
-    }
-}
-
-void timer_callback() {
-    // Set TRIG pin low
-    gpio_put(TRIG_PIN, 0);
-    
-    // Disable the timer interrupt (single shot)
-    hw_clear_bits(&timer_hw->intr, 1u << 0);
-}
-
-void hcsr04_init() {
-    gpio_init(TRIG_PIN);
-    gpio_set_dir(TRIG_PIN, GPIO_OUT);
-    gpio_put(TRIG_PIN, 0);
-
-    gpio_init(ECHO_PIN);
-    gpio_set_dir(ECHO_PIN, GPIO_IN);
-
-    // Setup interrupt on ECHO pin for both rising and falling edges
-    gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &echo_isr);
-}
-
-void hcsr04_trigger_measurement() {
-    // Start the timer interrupt for 10 microseconds
-    uint alarm_num = 0;  // Using timer 0
-    hw_set_bits(&timer_hw->inte, 1u << alarm_num);
-    timer_hw->alarm[alarm_num] = timer_hw->timerawl + 10;  // Set for 10 microseconds from now
-    
-    irq_set_exclusive_handler(TIMER_IRQ_0, timer_callback);
-    irq_set_enabled(TIMER_IRQ_0, true);
-
-    // Set TRIG pin high
-    gpio_put(TRIG_PIN, 1);
-}
-
-
-float hcsr04_calculate_distance_cm() {
-    float time_elapsed = (float)(end_time - start_time);  // Time in microseconds
-    return (time_elapsed * 0.0343) / 2;  // Speed of sound is 343 m/s or 0.0343 cm/us
-}
-
 
 void
 set_pulse_width(uint slice_num, uint8_t channel, float duty_cycle)
@@ -135,16 +76,16 @@ moveBackward()
     gpio_put(RIGHT_BACKWARD, 1);
     gpio_put(RIGHT_FORWARD, 0);
     gpio_put(LEFT_BACKWARD, 1);
-    gpio_put(LEFT_FORWARD, 0);
+    gpio_put(5, 0);
 }
 void
 stopMovement()
 {
     printf("Stop!");
-    gpio_put(RIGHT_BACKWARD, 0);
-    gpio_put(RIGHT_FORWARD, 0);
-    gpio_put(LEFT_BACKWARD, 0);
-    gpio_put(LEFT_FORWARD, 0);
+    gpio_put(2, 0);
+    gpio_put(3, 0);
+    gpio_put(4, 0);
+    gpio_put(5, 0);
 }
 
 int
@@ -155,21 +96,23 @@ main()
     // Initialize the standard I/O for printf
     stdio_init_all();
     adc_init();
-    gpio_init(RIGHT_BACKWARD);
-    gpio_init(RIGHT_FORWARD);
-    gpio_init(LEFT_BACKWARD);
-    gpio_init(LEFT_FORWARD);
+    gpio_init(2);
+    gpio_init(3);
+    gpio_init(4);
+    gpio_init(5);
 
     struct repeating_timer timer;
     // Tell GPIO 0 and 1 they are allocated to the PWM
-    gpio_set_function(RIGHT_PWM, GPIO_FUNC_PWM);
-    gpio_set_function(LEFT_PWM, GPIO_FUNC_PWM);
+    gpio_set_function(0, GPIO_FUNC_PWM);
+    gpio_set_function(1, GPIO_FUNC_PWM);
+    // Set ADC input pin
+    gpio_set_function(26, GPIO_FUNC_SIO);
 
-    gpio_set_dir(RIGHT_BACKWARD, GPIO_OUT);
-    gpio_set_dir(RIGHT_FORWARD, GPIO_OUT);
-    gpio_set_dir(LEFT_BACKWARD, GPIO_OUT);
-    gpio_set_dir(LEFT_FORWARD, GPIO_OUT);
-    uint slice_num = pwm_gpio_to_slice_num(RIGHT_PWM);
+    gpio_set_dir(2, GPIO_OUT);
+    gpio_set_dir(3, GPIO_OUT);
+    gpio_set_dir(4, GPIO_OUT);
+    gpio_set_dir(5, GPIO_OUT);
+    uint slice_num = pwm_gpio_to_slice_num(0);
     pwm_set_clkdiv(slice_num, 100);
     // Set period of 4 cycles (0 to 3 inclusive)
     pwm_set_wrap(slice_num, 12500);
@@ -183,16 +126,22 @@ main()
 
     while (1)
     {
-        if (time_us_32() - last_trigger_time > 500000) {
-            hcsr04_trigger_measurement();
-            last_trigger_time = time_us_32();
-        }
-
-        // Check if a new measurement is available
-        if (new_measurement_available) {
-            float distance = hcsr04_calculate_distance_cm();
-            printf("Distance: %.2f cm\n", distance);
-            new_measurement_available = false;
-        }
+        sleep_ms(5000);
+        moveForward();
+        sleep_ms(5000);
+        set_pulse_width(slice_num, PWM_CHAN_A,50);
+        set_pulse_width(slice_num, PWM_CHAN_B,50);
     }
 }
+// sleep_ms(5000);
+//         moveForward();
+//         sleep_ms(5000);
+//         stopMovement();
+//         sleep_ms(5000);
+//         moveBackward();
+//         sleep_ms(5000);
+//         turnLeft();
+//         sleep_ms(5000);
+//         turnRight();
+//         set_pulse_width(slice_num, PWM_CHAN_A,50);
+//         set_pulse_width(slice_num, PWM_CHAN_B,50);
